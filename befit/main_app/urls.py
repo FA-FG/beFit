4f4531f28cc771f5e3507b1d873f444461f5d268
Fatima <fatoomi.1999@gmail.com>
2025-01-15 19:37:19 +0300

    gym crud

--- befit/main_app/urls.py
+++ befit/main_app/urls.py
@@ -4,8 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('classes/', views.class_index, name='index'),
-    path('accouts/signup/', views.signup, name="signup")
+    path('gyms/', views.class_index, name='index'),
+    path('accouts/signup/', views.signup, name="signup"),
+    path('gyms/<int:gym_id>/', views.gyms_detail, name="detail"),
+    path('gyms/create/', views.GymCreate.as_view(), name='gyms_create'),
+    # CBV's
+    path('gyms/<int:pk>/update', views.GymUpdate.as_view(), name='gyms_update'),
+    path('gyms/<int:pk>/delete', views.GymDelete.as_view(), name='gyms_delete'),
 
   
     
