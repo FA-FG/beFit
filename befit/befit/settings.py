0dded981f69cf17d9265cab09d8db3076a599515
Fatima <fatoomi.1999@gmail.com>
2025-01-15 14:04:31 +0300

    app setup

--- befit/befit/settings.py
+++ befit/befit/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,8 +81,8 @@ WSGI_APPLICATION = 'befit.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'befit',
-        'USER': 'fatima',
+        'NAME': os.getenv('DATABASENAME'),
+        'USER': os.getenv('DATABASEUSER'),
     }
 }
 
