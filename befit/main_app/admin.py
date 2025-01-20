be61c1c96022b0424f87520051fd6872b5f58041
Fatima <fatoomi.1999@gmail.com>
2025-01-20 10:28:52 +0300

    almost completed

--- befit/main_app/admin.py
+++ befit/main_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Gym, Session, Profile
+from .models import Gym, Session, Profile,Subscription,SubscriptionPackage
+
 
 # Register your models here.
 admin.site.register(Gym)
 admin.site.register(Session)
 admin.site.register(Profile)
+admin.site.register(Subscription)
+admin.site.register(SubscriptionPackage)
