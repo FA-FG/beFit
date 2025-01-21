55663e10b0fdd6ca05e8bdbf6d11c55d56019d46
nooraljohar <nooraljohar.a72@gmail.com>
2025-01-21 23:33:13 +0300

    css

--- befit/befit/settings.py
+++ befit/befit/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
         'NAME': os.getenv('DATABASENAME'),
         'USER': os.getenv('DATABASEUSER'),
         'PASSWORD': os.getenv('DATABASEPASSWORD'),
+        'PORT': os.getenv('DATABASEPORT')
     }
 }
 
